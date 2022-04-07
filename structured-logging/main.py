d3c27bf85f0a6961e82f7faa496876ae65be4f47
Ryan Gates <ryan.gates@ukg.com>
2022-04-07 12:14:27 -0400

    used standar log format

--- structured-logging/main.py
+++ structured-logging/main.py
@@ -3,6 +3,7 @@ import json
 import logging
 import traceback
 import sys
+import socket
 
 APP_NAME = 'structured-logging'
 APP_VERSION = 'effda69c518a352371848690b3676738747ef776'
@@ -39,21 +40,28 @@ def record_factory(*args, **kwargs) -> logging.LogRecord:
 
     record.json_formatted = json.dumps(
     {
-      'level': record.levelname,
-      'unixtime': record.created,
-      'thread': record.thread,
-      'location': '{}:{}:{}'.format(
-        record.pathname or record.filename,
-        record.funcName,
-        record.lineno,
-      ),
-      'exception': record.exc_info,
-      'traceback': traceback.format_exception(*record.exc_info) if record.exc_info else None,
-      'app': {
-        'name': APP_NAME,
-        'releaseId': APP_VERSION,
-        'message': record.getMessage(),
-      },
+        'uslog': {
+            'timestamp': record.created,
+            'hostname': socket.gethostname(),
+            'service_id': APP_NAME,
+            'service_version': APP_VERSION,
+            'service_commit_hash': 'tbd',
+            'log_version': '1.0.1',
+            'log_level': record.levelname,
+            'logger_name': record.funcName,
+            'log_type': 'logs',
+            'security_context': '',
+            'exception': record.exc_info,
+            'trace': traceback.format_exception(*record.exc_info) if record.exc_info else None,
+            
+            'thread': record.thread,
+            'message': record.getMessage(),
+            'location': '{}:{}:{}'.format(
+                record.pathname or record.filename,
+                record.funcName,
+                record.lineno,
+            )
+        }
     },
         cls=JsonEncoderDatetime,
     )
